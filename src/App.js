b07b3e0368d8cbf05997a4e7eb3b47b7959ea89d
Yelison <ortizyelison@gmail.com>
2022-04-09 13:36:05 -0400

    First commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { render } from "react-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { StrictMode, useState } from "react";
+import ThemeContext from "./ThemeContext";
+import SearchParams from "./searchParams";
+import Details from "./Details";
+
+const App = () => {
+  const theme = useState("darkblue");
+  return (
+    <StrictMode>
+      <ThemeContext.Provider value={theme}>
+        <BrowserRouter>
+          <header>
+            <Link to="/">Adopt Me</Link>
+          </header>
+          <Routes>
+            <Route path="/" element={<SearchParams />} />
+            <Route path="/details/:id" element={<Details />} />
+          </Routes>
+        </BrowserRouter>
+      </ThemeContext.Provider>
+    </StrictMode>
+  );
+};
+
+render(<App />, document.getElementById("root"));
